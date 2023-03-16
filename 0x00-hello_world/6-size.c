0a078c4801a5bdbb3aa4f7e70e8fc8f4fdb7f44d
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-16 10:34:45 -0700

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,8 @@ int main(void)
 {
 printf("size of a char:%lu byte(s)", sizeof(char));
 printf("size of an int:%lu byte(s)", sizeof(int));
-printf("size of a long int:%lu byte(s)", sizeof(unsigned  int));
+printf("size of a long int:%lu byte(s)", sizeof(long int));
+printf("size of a long long int:%lu byte(s)", sizeof(long long int));
 printf("size of a float:%lu byte(s)", sizeof(float));
-printf("size of a float:%lu byte(s)", sizeof(float));
-printf("size of a float:%lu byte(s)", sizeof(float));
-printf("size of a double:%lu byte(s)", sizeof(double));
-printf("size of a unsigned int:%lu byte(s)", sizeof(unsigned int));
 return (0);
 }
