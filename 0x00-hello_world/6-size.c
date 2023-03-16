0b967a913b0bf693269908f4d24013d3165ecaeb
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-16 07:45:08 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return 0  on Success
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof (char));
+	printf("%lu\n", sizeof (int));
+	printf("%lu\n", sizeof (long int));
+	printf("%lu\n", sizeof (long long int));
+	printf("%lu\n", sizeof (float));
+	return (0);
+}
+	
