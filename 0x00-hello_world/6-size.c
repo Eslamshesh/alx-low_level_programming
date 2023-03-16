cf016ba2230bea50d5213fcebafae4f3a4fe982f
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-16 10:25:34 -0700

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-printf("size of a char:%lu byte(s)", sizeof (char));
-printf("size of int:%lu byte(s)", sizeof (int));
-printf("size of unsigned int  :%lu byte(s)", sizeof (unsigned  int));
-printf("size of a float:%lu byte(s)", sizeof (float));
-printf("size of a float:%lu byte(s)", sizeof (float));
-printf("size of a float:%lu byte(s)", sizeof (float));
-printf("size of a float:%lu byte(s)", sizeof (double));
-printf("size of a float:%lu byte(s)", sizeof (unsigned int));
-return (0);
+printf("size of a char:%lu byte(s)", sizeof(char));
+printf("size of int:%lu byte(s)", sizeof(int));
+printf("size of unsigned int  :%lu byte(s)", sizeof(unsigned  int));
+printf("size of a float:%lu byte(s)", sizeof(float));
+printf("size of a float:%lu byte(s)", sizeof(float));
+printf("size of a float:%lu byte(s)", sizeof(float));
+printf("size of a float:%lu byte(s)", sizeof(double));
+printf("size of a float:%lu byte(s)", sizeof(unsigned int));
+return (0)
 }
-	
