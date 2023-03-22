6e092b2f89539535bc0b4b80754d9b306d5c0f83
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-22 04:21:11 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+return (0);
+}
