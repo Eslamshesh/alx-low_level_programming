3fe29f3df144d6b45034a6dc015e7d672b87f0e3
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-22 13:10:01 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - _alphabet_x10
+ *
+ * Return: o (success)
+ */
+void print_alphabet_x10(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+return (0);
+}
