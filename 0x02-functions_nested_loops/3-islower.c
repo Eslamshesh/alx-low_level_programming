7f52f8c66a8e191f0ab07e7a84db78783d929b11
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-22 13:35:32 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _slower - check if achar is alowercase
+ *
+ * Return: 1 if char is alowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c >= 'z')
+return (1);
+else
+return (0);
+}
