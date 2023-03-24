f0f6ce6a9c322cb7f6bff3635d7b71917da28887
Eslamshesh <eslamshesh2004@gmail.com>
2023-03-24 02:21:10 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+int num, row, count;
+for (row = 1; row <= 10; row++)
+{
+for (count = 0; count <= 14; count++)
+{
+num = count;
+if (count < 0)
+{
+_putchar(1 + 48);
+num = count % 10;
+}
+_putchar(num + 48);
+}
+_putchar('\n');
+}
+}
